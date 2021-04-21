2969a621ba42db72890babbfaff477f222aeffea
Nazarii Ihnat <ignatnazar@gmail.com>
2021-04-21 15:47:25 +0200

    Nazar i Kamil

--- reservation/src/main/java/com/bootcamp/reservation/api/ReservationApi.java
+++ reservation/src/main/java/com/bootcamp/reservation/api/ReservationApi.java
@@ -1,17 +1,43 @@
 package com.bootcamp.reservation.api;
 
+import com.bootcamp.reservation.model.Reservation;
+import com.bootcamp.reservation.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.websocket.server.PathParam;
+import java.util.UUID;
+
 @RestController
 @EnableSwagger2
 @RequestMapping(path = "/reservation")
-public class ReservationApi {
+public class
+ReservationApi {
 
-    @GetMapping
-    public String helloWorld(){
-        return "hello";
+    private final ReservationRepository reservationRepository;
+
+    public ReservationApi(ReservationRepository reservationRepository) {
+        this.reservationRepository = reservationRepository;
+    }
+
+    @PostMapping("/create")
+    public void saveReservation(Reservation reservation) {
+        reservationRepository.createReservation(reservation);
+    }
+
+    @GetMapping("/{id}")
+    public String getReservation(@PathParam("id") UUID id) {
+        reservationRepository.findReservation(id);
+        return "index";
+    }
+
+    @GetMapping("/all")
+    public String getAllReservations(){
+        reservationRepository.findAllReservations();
+        return "index";
     }
 }
--- reservation/src/main/java/com/bootcamp/reservation/repository/ReservationRepository.java
+++ reservation/src/main/java/com/bootcamp/reservation/repository/ReservationRepository.java
@@ -1,6 +1,8 @@
 package com.bootcamp.reservation.repository;
 
 import com.bootcamp.reservation.model.Reservation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
