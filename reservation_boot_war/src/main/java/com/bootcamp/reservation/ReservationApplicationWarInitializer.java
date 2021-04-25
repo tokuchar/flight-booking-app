98bab1ceae8b152a5f20b9f696dd5dc0f5ac4648
damianSak <damian.sak88@gmail.com>
2021-04-26 00:24:13 +0200

    working version

---
+++ reservation_boot_war/src/main/java/com/bootcamp/reservation/ReservationApplicationWarInitializer.java
@@ -0,0 +1,14 @@
+package com.bootcamp.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ReservationApplication extends SpringBootServletInitializer {
+
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationApplication.class, args);
+    }
+}
